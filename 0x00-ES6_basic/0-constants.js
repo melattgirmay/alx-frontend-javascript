e73e80311a867e0863ed0a9e727d3a1cbc48f739
Melat Hailu <melatgirmay20@gmail.com>
2023-09-25 10:04:11 +0300

    Remove  ES6 Module Syntax

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,19 +1,16 @@
-// Specify ES6 module syntax
-"use strict";
-
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
   
